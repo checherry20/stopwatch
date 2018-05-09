9751222d0b52a0f2303a45b3e6081016fe7bf680
Cherry <cherrilynablay.20@gmail.com>
2018-05-09 20:38:45 +0800

    First commit

---
+++ StopwatchExercise/src/StopwatchExercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StopwatchExercise
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var stopwatch = new StopwatchControl();
+
+            String rl;
+            int ctr = 0;
+            do
+            {
+                ctr++;
+                
+                Console.WriteLine("Start time: {0}", stopwatch.GetStartTime());
+                Console.WriteLine("Stoped: {0}", stopwatch.Stop(DateTime.Now));
+                Console.WriteLine("Duration is {0}", stopwatch.GetDuration());
+
+                Thread.Sleep(2000);
+
+                stopwatch.Start(stopwatch.GetStopTime());
+
+                rl = Console.ReadLine();
+            } while (rl != null);
+        }
+    }
+}
---
+++ StopwatchExercise/src/StopwatchExercise/StopwatchControl.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StopwatchExercise
+{
+    public class StopwatchControl
+    {
+        private DateTime _startTime;
+        private TimeSpan _duration; 
+        //private DateTime _stopTime;
+        private DateTime _stopTime; //{ get; set; }
+
+        public StopwatchControl()
+        {
+            this._startTime = DateTime.Now;
+        }
+
+        public DateTime Start(DateTime dateTime)
+        {
+            this.SetStartTime(dateTime);
+            return this._startTime;
+        }
+
+        public DateTime Stop(DateTime dateTime)
+        {
+            this._stopTime = dateTime;
+            this.SetDuration();
+            return this._stopTime;
+        }
+        
+        public void SetDuration()
+        {
+            this._duration = this._stopTime.Subtract(this._startTime);
+        }
+
+        public TimeSpan GetDuration()
+        {
+            return this._duration;
+        }
+
+        public void SetStartTime(DateTime dateTime)
+        {
+            this._startTime = dateTime;
+        }
+
+        public DateTime GetStartTime()
+        {
+            return this._startTime;
+        }
+
+        public void SetStopTime(DateTime dateTime)
+        {
+            this._stopTime = dateTime;
+        }
+
+        public DateTime GetStopTime()
+        {
+            return this._stopTime;
+        }
+    }
+}
